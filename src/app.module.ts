6e43c4f8b9222c29f42993a58db0869cccc95a59
alad1n0 <alad1nfaceit.al@gmail.com>
2025-02-22 16:01:44 +0200

    addUpdateProject

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import {PrismaService} from "./prisma/prisma.service";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
