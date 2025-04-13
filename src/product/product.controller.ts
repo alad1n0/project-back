f6449ed400ec86ad2ecbc242638493ba89df6d53
RiabushenkoA <ariabushenko@blackbird.io>
2025-04-13 23:39:32 +0300

    Added some crud operations

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -5,4 +5,19 @@ import { Product } from '@prisma/client';
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+
+  @Get('get-all')
+  async findAll() {
+    const products = await this.productService.findAllProducts();
+    return products;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const product = await this.productService.findProductById(id);
+    return product;
+  }
 }
+
+
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+
+  async findAllProducts() {
+
+    const products = await this.prisma.product.findMany({
+      include: {
+        sizes: true,
+        options: true,
+        category: true,
+        subcategory: true 
+      },
+    });
+
+    const formattedProducts = products.map(product => ({
+      ...product,
+      image: product.image ? `${process.env.FILE_BASE_URL}/${product.image}` : null,
+    }));
+
+    return formattedProducts;
+  }
+
+  async findProductById(id: string) {
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      include: {
+        sizes: true,
+        options: true,
+        category: true,
+        subcategory: true
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Продукт з id: ${id} не знайдено`);
+    }
+
+    return {
+      ...product,
+      image: product.image ? `${process.env.FILE_BASE_URL}/${product.image}` : null,
+    };
+  }
+}
