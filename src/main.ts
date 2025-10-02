ea80b00fc54a8c3e28769589744a19e674d74536
alad1n0 <alad1nfaceit.al@gmail.com>
2025-10-02 11:22:38 +0300

    addUpdateProject

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,19 @@ dotenv.config();
 
     const app = await NestFactory.create(AppModule);
 
+    // app.use((req, res, next) => {
+    //   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    //   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, Accept');
+    //   res.header('Access-Control-Allow-Credentials', 'true');
+    //
+    //   if (req.method === 'OPTIONS') {
+    //     return res.sendStatus(204);
+    //   }
+    //
+    //   next();
+    // });
+
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
@@ -19,6 +32,9 @@ dotenv.config();
 
     app.enableCors({
         origin: true,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Authorization', 'Content-Type', 'Accept'],
     });
 
     app.setGlobalPrefix('api');
